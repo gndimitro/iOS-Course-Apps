6c92bbacfe6c9e6697f6367e8a01f26f9ec8ed59
George <george.n.dimitrov@gmail.com>
2016-07-26 16:48:50 -0600

    Implemented the shake and swipe features for the lesson

--- Swipes & Shakes/Swipes & Shakes/ViewController.swift
+++ Swipes & Shakes/Swipes & Shakes/ViewController.swift
@@ -12,7 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swiped(_:)))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            print("Device was shaken")
+        }
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+            
+            case UISwipeGestureRecognizerDirection.Right:
+                print("Swiped Right")
+            
+            case UISwipeGestureRecognizerDirection.Up:
+                print("Swiped Up")
+            
+            default:
+                break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
