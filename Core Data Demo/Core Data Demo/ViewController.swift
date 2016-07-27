3482b4fa9894c24939d664796d33ec9203ba35e0
George <george.n.dimitrov@gmail.com>
2016-07-26 23:35:03 -0600

    Finished the core data example: - Added an item to a basic entity - Fetched the item and print the data

--- Core Data Demo/Core Data Demo/ViewController.swift
+++ Core Data Demo/Core Data Demo/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+         //Commented code since data was added, uncomment if need to re-add data to database
+        
+        /*let newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("George", forKey: "username")
+        
+        newUser.setValue("pass123", forKey: "password")
+        
+        do {
+        
+            try context.save()
+        
+        } catch {
+            
+            //Handle errors here
+            print("There was an issue adding item to database")
+        
+        }*/
+        
+        let request = NSFetchRequest(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    print(result.valueForKey("username")!)
+                    print(result.valueForKey("password")!)
+                }
+                
+            }
+            
+        } catch {
+            
+            print("Fetch failed")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
