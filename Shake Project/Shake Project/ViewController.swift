62e130b7b8feaf15af679d3ee0c335c56da6f0fe
George <george.n.dimitrov@gmail.com>
2016-07-26 23:04:26 -0600

    Finished the project: - Shake the phone and then you'll hear a random sound play

--- Shake Project/Shake Project/ViewController.swift
+++ Shake Project/Shake Project/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player = AVAudioPlayer()
+    
+    var sounds = ["bean", "belch", "giggle", "pew", "pig", "snore", "static", "uuu"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            
+            let randomNumber = arc4random_uniform(UInt32(sounds.count))
+            
+            let fileLocation = NSBundle.mainBundle().pathForResource(sounds[Int(randomNumber)], ofType: "mp3")
+            
+            do {
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                player.play()
+            } catch {
+                
+                //Catch error here
+            
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
